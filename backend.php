ee2e253cbdf39306ef61f559409cbffb1f9f57d2
Shayan Esmaili <esmailishayan15@gmail.com>
2023-09-11 14:18:58 +0330

    Changes

---
+++ backend.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    
+</head>    
+<body>
+    <?php 
+    if(!isset($_POST['fname'])){
+        
+    }
+    ?>
+    <div class="container">
+        <div class="item"><?php echo $_POST['fname'] ?> </div>
+        <div class="item"><?php  echo $_POST['lname'] ?> </div>
+    </div>
+</body>
+</html>
+
